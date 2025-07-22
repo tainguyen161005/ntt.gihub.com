692094580b2ab4a4e582fd23069e15c7b6af61ef
tainguyen161005 <2311558489@nttu.edu.vn>
2025-07-22 14:45:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,66 +5,65 @@ const hangmanImage = document.querySelector(".hangman-box img");
 const gameModal = document.querySelector(".game-modal");
 const playAgainBtn = gameModal.querySelector("button");
 
-// Game variables
 let currentWord, currentFullWord, correctLetters, wrongGuessCount;
 const maxGuesses = 6;
 
 const resetGame = () => {
-    correctLetters = [];
-    wrongGuessCount = 0;
-    hangmanImage.src = "images/hangman-0.svg";
-    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
-    wordDisplay.innerHTML = currentWord
-        .split("")
-        .map(() => `<li class="letter"></li>`)
-        .join("");
-    keyboardDiv.querySelectorAll("button").forEach((btn) => (btn.disabled = false));
-    gameModal.classList.remove("show");
+  correctLetters = [];
+  wrongGuessCount = 0;
+  hangmanImage.src = "images/hangman-0.svg";
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+  wordDisplay.innerHTML = currentWord
+    .split("")
+    .map(() => `<li class="letter"></li>`)
+    .join("");
+  keyboardDiv.querySelectorAll("button").forEach((btn) => (btn.disabled = false));
+  gameModal.classList.remove("show");
 };
 
 const getRandomWord = () => {
-    const { word, fullWord, hint } = wordList[Math.floor(Math.random() * wordList.length)];
-    currentWord = word;         // không dấu (để chơi)
-    currentFullWord = fullWord; // có dấu (để hiển thị đáp án)
-    document.querySelector(".hint-text b").innerText = hint;
-    resetGame();
+  const { word, fullWord, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+  currentWord = word; // không dấu để chơi
+  currentFullWord = fullWord; // có dấu để hiện đáp án
+  document.querySelector(".hint-text b").innerText = hint;
+  resetGame();
 };
 
 const gameOver = (isVictory) => {
-    const modalText = isVictory ? `🎉 Bạn đã tìm được từ:` : "😢 Đáp án đúng là:";
-    gameModal.querySelector("img").src = `images/${isVictory ? "victory" : "lost"}.gif`;
-    gameModal.querySelector("h4").innerText = isVictory ? "Chúc mừng!" : "Thua rồi!";
-    gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentFullWord}</b>`;
-    gameModal.classList.add("show");
+  const modalText = isVictory ? `🎉 Bạn đã tìm được từ:` : "😢 Đáp án đúng là:";
+  gameModal.querySelector("img").src = `images/${isVictory ? "victory" : "lost"}.gif`;
+  gameModal.querySelector("h4").innerText = isVictory ? "Chúc mừng!" : "Thua rồi!";
+  gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentFullWord}</b>`;
+  gameModal.classList.add("show");
 };
 
 const initGame = (button, clickedLetter) => {
-    if (currentWord.includes(clickedLetter)) {
-        [...currentWord].forEach((letter, index) => {
-            if (letter === clickedLetter) {
-                correctLetters.push(letter);
-                const li = wordDisplay.querySelectorAll("li")[index];
-                li.innerText = letter;
-                li.classList.add("guessed");
-            }
-        });
-    } else {
-        wrongGuessCount++;
-        hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
-    }
-    button.disabled = true;
-    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+  if (currentWord.includes(clickedLetter)) {
+    [...currentWord].forEach((letter, index) => {
+      if (letter === clickedLetter) {
+        correctLetters.push(letter);
+        const li = wordDisplay.querySelectorAll("li")[index];
+        li.innerText = letter;
+        li.classList.add("guessed");
+      }
+    });
+  } else {
+    wrongGuessCount++;
+    hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+  }
+  button.disabled = true;
+  guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
 
-    if (wrongGuessCount === maxGuesses) return gameOver(false);
-    if (correctLetters.length === currentWord.length) return gameOver(true);
+  if (wrongGuessCount === maxGuesses) return gameOver(false);
+  if (correctLetters.length === currentWord.length) return gameOver(true);
 };
 
 // Tạo bàn phím a-z
 for (let i = 97; i <= 122; i++) {
-    const button = document.createElement("button");
-    button.innerText = String.fromCharCode(i);
-    keyboardDiv.appendChild(button);
-    button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
+  const button = document.createElement("button");
+  button.innerText = String.fromCharCode(i);
+  keyboardDiv.appendChild(button);
+  button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
 }
 
 getRandomWord();
